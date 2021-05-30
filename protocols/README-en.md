9759e963275aa1952eb321d6ae20ae3ffe41420d
Remi Gau <remi_gau@hotmail.com>
2021-05-30 22:52:37 +0200

    transfer jsonld to eCOBIDAS repo

--- protocols/README-en.md
+++
@@ -1,1 +0,0 @@
-# Landing page for the template protocol
--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/Remi-Gau/artemis_checklist/master/protocols/artemis/artemis_schema',
-  banner: 'This protocol is under construction.',
-  startButton: 'Start',
-  assetsPublicPath: '/artemis_checklist/',
-  backendServer: 'null'
-}
+  githubSrc:
+    "https://raw.githubusercontent.com/ohbm/eCOBIDAS/master/protocols/artemis/artemis_schema",
+  banner: "This protocol is under construction.",
+  startButton: "Start",
+  assetsPublicPath: "/artemis_checklist/",
+  backendServer: "null",
+};
