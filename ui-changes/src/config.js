769b29371f6060558e375addb8d7a65a8b1db77d
Remi Gau <remi_gau@hotmail.com>
2021-06-25 09:42:32 +0200

    update to new schema

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /* eslint-disable */
   githubSrc:
-    "https://raw.githubusercontent.com/ohbm/eCOBIDAS/master/protocols/artemis/artemis_schema",
+    "https://raw.githubusercontent.com/ohbm/eCOBIDAS/master/schemas/artemis/protocols/artemis_schema.jsonld",
   banner: "This protocol is under construction.",
   startButton: "Start",
   assetsPublicPath: "/artemis_checklist/",
