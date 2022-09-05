0c2527930bf69bcfda0b35cdf30eb0d2e661a2c4
Remi Gau <remi_gau@hotmail.com>
2022-09-05 13:40:23 +0200

    Update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /* eslint-disable */
   githubSrc:
-    "https://raw.githubusercontent.com/INCF/artem-is/main/schemas/artemis/protocols/artemis_schema.jsonld",
+    "https://raw.githubusercontent.com/INCF/artem-is/main/schemas/artemis/artemis_schema.jsonld",
   banner: "This protocol is under construction.",
   startButton: "Start",
   assetsPublicPath: "/artemis_checklist/",
